e383633d1c3ac0483e0a9bea4c6fbff2c6761958
Ainoha Barcia <ainoha.barcia@gmail.com>
2023-11-02 09:21:54 +0100

    ejercicios corregidos

--- src/main/java/n1exercici3/OutOfBounds.java
+++ src/main/java/n1exercici3/OutOfBounds.java
@@ -5,7 +5,13 @@ import java.util.ArrayList;
 public class OutOfBounds {
 
     public String getElementAtIndex(ArrayList<String> nameList, int index) {
+        if (index < 0 || index >= nameList.size()) {
+            throw new ArrayIndexOutOfBoundsException("Índice fuera del rango del array");
+        }
+        String name = nameList.get(index);
         return nameList.get(index);
     }
 
-}
+    }
+
+
